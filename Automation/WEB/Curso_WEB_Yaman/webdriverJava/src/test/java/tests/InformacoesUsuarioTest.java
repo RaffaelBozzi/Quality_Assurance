73f7cc0554471bfdd66fface16392f198fd3801a
Raffael Bozzi <raffaelbozzi@gmail.com>
2022-07-15 09:32:28 -0300

    Inicio do curso de automação web

---
+++ Automation/WEB/Curso_WEB_Yaman/webdriverJava/src/test/java/tests/InformacoesUsuarioTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InformacoesUsuarioTest {
+  @Test
+  public void testAdicionarUmaInformacaoAdicionalDoUsuario() {
+    Assert.assertEquals(1, 1);
+  }
+}
